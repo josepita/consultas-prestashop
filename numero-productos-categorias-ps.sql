e68717fe03e89db0e2012801db07359ddab976ac
josepita <josepita@gmail.com>
2019-03-14 07:46:34 +0100

    Update numero-productos-categorias-ps.sql

--- numero-productos-categorias-ps.sql
+++ numero-productos-categorias-ps.sql
@@ -2,5 +2,5 @@ select cl.id_category,
      cl.name,
      (select count(*) from ps_category_product cp where cp.id_category = cl.id_category) as total
 from ps_category_lang cl
-where cl.id_lang = 1 where cl.name like  "%%"
+where cl.id_lang = 1 and cl.name like  "%%"
 Order by total Desc
